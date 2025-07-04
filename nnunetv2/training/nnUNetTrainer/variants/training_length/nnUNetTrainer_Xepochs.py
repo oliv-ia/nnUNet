f39a6fa1777a9998269ec98a3443a9570c8921f1
Olivia Murray <olivianmurray1999@gmail.com>
2025-07-04 12:09:06 +0100

    Added 400 epoch trainer

--- nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
+++ nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
@@ -54,6 +54,11 @@ class nnUNetTrainer_250epochs(nnUNetTrainer):
         super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 250
 
+class nnUNetTrainer_400epochs(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_epochs = 400
 
 class nnUNetTrainer_500epochs(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
